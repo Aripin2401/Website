eb7c3a5a92079a3c2a753e930b12eb83138c4066
Ujang Aripin <47048167+Aripin2401@users.noreply.github.com>
2021-12-29 18:23:41 +0700

    Delete CRUD-Dengan-Bootstrap-Input-Siswa directory

--- CRUD-Dengan-Bootstrap-Input-Siswa/koneksi/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server = "localhost"; //nama server xampp
-$username = "root"; //nama username phpmyadmin bawaan xampp
-$password = ""; //password phpmyadmin bawaan xampp memang kosong
-$database = "belajar-crud"; //nama database
- 
-mysql_connect($server, $username, $password) or die("Koneksi ke database gagal!"); //koneksi phpmyadmin ke php
-mysql_select_db($database) or die("Tidak ada database yang dipilih!"); //koneksi database ke website php
-?>
